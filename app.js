0b27772a8fc88c47faba310f389d2268b2a6bb54
benja2602 <benjaacu26@gmail.com>
2021-12-02 00:04:07 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('¡Servidor funcionando!');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+})
